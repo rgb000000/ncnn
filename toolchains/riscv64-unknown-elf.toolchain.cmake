90c5fccb8a8886c881eaff8e462eb42965ae7d64
rgb000000 <rgb000000@gmail.com>
2022-05-01 15:55:48 +0800

    fix: let ncnn compile to mycpu, add compile and link option

--- toolchains/riscv64-unknown-elf.toolchain.cmake
+++ toolchains/riscv64-unknown-elf.toolchain.cmake
@@ -14,6 +14,34 @@ set(CMAKE_CXX_COMPILER "${RISCV_ROOT_PATH}/bin/riscv64-unknown-elf-g++")
 
 set(CMAKE_FIND_ROOT_PATH "${RISCV_ROOT_PATH}/riscv64-unknown-elf")
 
+add_compile_options(
+        -march=rv64ima -mabi=lp64 -mcmodel=medany
+        -nostartfiles
+        -Wall -c
+        -ffunction-sections
+        -nostdlib
+        -fno-use-cxa-atexit)
+# SET(CMAKE_CXX_FLAGS "-march=rv64ima -mabi=lp64 -mcmodel=medany -nostartfiles -Wall -nostdlib" )
+# SET(CMAKE_C_FLAGS   "-march=rv64ima -mabi=lp64 -mcmodel=medany -nostartfiles -Wall -nostdlib" )
+
+# add_link_options(
+#                  -L/opt/rv64ima/lib/gcc/riscv64-unknown-elf/11.1.0
+#                  -L/opt/rv64ima/riscv64-unknown-elf/lib
+#                  -Wl,-nostdlib
+#                  -Wl,--start-group -l:libc.a -l:libm.a -l:libgcc.a -Wl,--end-group
+#                  -Wl,--gc-sections --verbose
+#         )
+
+# SET(CMAKE_EXE_LINKER_FLAGS " -e _mystart                              \
+#                              -nostdlib                                \
+#                              -T /home/PRJ/mycpu/ncnn/src/internal.ld  \
+#                              /home/PRJ/mycpu/newlib_test/mycrt0.o     \
+#                              /home/PRJ/mycpu/newlib_test/stubs.o      \
+#                              -L/opt/rv64ima/lib/gcc/riscv64-unknown-elf/11.1.0 \
+#                              -L/opt/rv64ima/riscv64-unknown-elf/lib            \
+#                              -Wl,-\( -l:libc.a -l:libm.a -l:libgcc.a -Wl,-\)   \
+#                              -Wl,--gc-sections --verbose")
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
